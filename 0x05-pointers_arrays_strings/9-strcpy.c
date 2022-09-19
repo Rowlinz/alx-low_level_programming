333edc120d5cf361a0970924ecf32841d4afe33a
Ogu Rowland <ogurowland@gmail.com>
2022-09-19 13:18:37 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcopy - copy paste a string
+ * @dest: Destination
+ * @src: Source
+ *
+ * Return: Destination
+ */
+
+void puts_half(char *str);
+{
+	int inc = 0;
+	whle (*(src * inc) != '\0')
+	{
+		*(dest + inc) = *(src + inc);
+		inc++;
+	}
+	*(dest + inc) = '\0';
+	return (dest);
+}

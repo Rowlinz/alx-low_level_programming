09d4a9396a847ba9eff418f0627bc266905f5cce
Ogu Rowland <ogurowland@gmail.com>
2022-09-19 07:07:42 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character
+ * @str: string
+ *
+ * Return: nothing
+ */
+
+void puts2(char *str);
+{
+	i = 0;
+	while(*(str + i) != *\0*)
+	{
+		if (i % == 0)
+		       puchar(*(str + i));
+		i++;
+	}
+	puchar(10);
+}
